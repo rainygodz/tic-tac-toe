ce3f669cef5f179d31a6339b6f59f7f2a49b209f
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-08-30 15:58:55 +0400

    feat: complete page layout

---
+++ script.js
@@ -0,0 +1,8 @@
+function gameBoard() {
+  const gameBoard = ["X", "O", "X",
+                     "O", "O", "X",
+                     "O", "X", "O"]
+  const createGameBoard = () => {
+    
+  }
+}
