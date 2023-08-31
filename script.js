884161248a131ff92065c4e909e4dbf0fcbe370d
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-08-31 15:24:23 +0400

    feat: implement renderGameBoard function

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function gameBoard() {
-  const gameBoard = ["X", "O", "X",
+const gameBoardDiv = document.querySelector(".gameBoard");
+const cells = document.querySelectorAll(".cell");
+
+
+const gameBoard = (function() {
+  const _gameBoard = ["X", "O", "X",
                      "O", "O", "X",
-                     "O", "X", "O"]
-  const createGameBoard = () => {
-    
+                     "O", "X", "O"];
+
+  const _getGameBoard = () => {
+    _gameBoard.forEach((cell, index) => {
+      cells[index].textContent = cell;
+    });
   }
-}
+
+  return {
+    renderGameBoard: function() {
+      _getGameBoard();
+    }
+  };
+})();
+
+gameBoard.renderGameBoard();
