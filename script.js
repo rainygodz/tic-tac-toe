1e3ffd8a5b612845b3bdf68d97196b8197f559b2
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-09-01 11:24:29 +0400

    feat: implement playRound method in game module

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const gameBoardDiv = document.querySelector(".gameBoard");
 const cells = document.querySelectorAll(".cell");
+const player1Score = document.querySelector("#p1-score");
+const player2Score = document.querySelector("#p2-score")
 
 
 const gameBoard = (function() {
-  const _gameBoard = ["", "", "",
+  let _gameBoard = ["", "", "",
                      "", "", "",
                      "", "", ""];
+
+  const isFull = () => {
+    let filled = true;
+    cells.forEach(cell => {
+      if (cell.textContent === "") {
+        filled = false;
+      }
+    });
+
+    return filled;
+  }
+
+  const resetGameBoard = () => {
+    _gameBoard = ["", "", "",
+                  "", "", "",
+                  "", "", ""];
+    _renderGameBoard();
+  }
 
   const _renderGameBoard = () => {
     _gameBoard.forEach((cell, index) => {
@@ -21,10 +41,39 @@ const gameBoard = (function() {
   }
 
   return {
-    playRound: function(event) {
-      addSymbol(event, player2);
+    addSymbol: function(event, player) {
+      addSymbol(event, player);
+    },
+    isFull: function() {
+      return isFull();
+    },
+    resetGameBoard: function() {
+      resetGameBoard();
     }
   };
+})();
+
+
+const game = (function() {
+  const player1 = playerFactory('Player1', "X");
+  const player2 = playerFactory('Player2', "O");
+  let p1Score = 0;
+  let p2Score = 0;
+
+  const playRound = (event) => {
+    console.log(gameBoard.isFull());
+    if (gameBoard.isFull()) {
+      gameBoard.resetGameBoard();
+    } else {
+      gameBoard.addSymbol(event, player1);
+    }
+  }
+
+  return {
+    playRound: function(event) {
+      playRound(event);
+    }
+  }
 })();
 
 
@@ -32,10 +81,6 @@ function playerFactory(name, symbol) {
   return {name, symbol};
 }
 
-const player1 = playerFactory('Player1', "X");
-const player2 = playerFactory('Player2', "O");
-
-
 cells.forEach(cell => {
-  cell.addEventListener("click", gameBoard.playRound);
+  cell.addEventListener("click", game.playRound);
 })
